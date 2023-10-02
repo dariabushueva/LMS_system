029409829e7d9246148f94413d884eb40ca040bc
dariabushueva <bushueva84@yandex.ru>
2023-10-02 19:40:50 +0300

    added views for user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from .views import *
+from users.views import *
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('user/create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('user/', UserListAPIView.as_view(), name='user_list'),
+    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('user/destroy/<int:pk>/', UserDestroyAPIView.as_view(), name='user_destroy'),
+]
